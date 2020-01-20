4309a79295b8976454b919e83574018929822a1b
Migbari <webdevel.bin@gmail.com>
2020-01-20 03:11:48 -0300

    tratou duplicidade

--- OmniStack/src/controllers/DevController.js
+++ OmniStack/src/controllers/DevController.js
@@ -4,34 +4,39 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 module.exports = {
-    async store (request, response) {
+    async store(request, response) {
         // Desestruturação de github_username pela request boby - corpo da requisição
-        const { github_username, techs, latitude, longitude } = request.body; 
-    
-        // Response da api do github 
-        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-    
-        // Desestruturação de pela apiResponse.
-        const {name = login, avatar_url, bio} = apiResponse.data
-        // console.log(name, avatar_url, bio, github_username);
-    
-        // Indica proximo item com split(','), percorre techs com map e .trim() remove espaços 
-        const techsArray = techs.split(',').map(techs => techs.trim());
-    
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude],
-        };
-    
-        const dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techsArray,
-            location,
-        });
-    
-    return response.json(dev)  
+        const { github_username, techs, latitude, longitude } = request.body;
+
+        // Tratar duplicidade no banco 
+        let dev = await Dev.findOne({ github_username });
+
+        if (!dev) {
+            // Response da api do github 
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+
+            // Desestruturação de pela apiResponse.
+            const { name = login, avatar_url, bio } = apiResponse.data
+            // console.log(name, avatar_url, bio, github_username);
+
+            // Indica proximo item com split(','), percorre techs com map e .trim() remove espaços 
+            const techsArray = techs.split(',').map(techs => techs.trim());
+
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+            };
+
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location,
+            });
+
+        }
+        return response.json(dev)
     }
 };
