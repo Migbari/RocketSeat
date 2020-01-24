12fd6868704c28d76ea9619dfc7ef26549186d70
Migbari <webdevel.bin@gmail.com>
2020-01-24 01:45:09 -0300

    adiciona cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express'); // Importou o modulo express
-const mongoose = require('mongoose'); // importar a biblioteca express para ter acesso a suas funcionalidades
+const mongoose = require('mongoose'); 
+const cors = require('cors')
 const routes = require('./routes'); // importou as rotas em route.js
+
 
 // Inicializar a aplicação, colocar o servidor no ar. Ou seja, criamos rota
 const app = express(); 
@@ -10,6 +12,7 @@ mongoose.connect('mongodb+srv://omnistack:miguelbatista@cluster0-w3uk9.mongodb.n
     useUnifiedTopology: true,
     useCreateIndex :  true ,
 });
+app.use(cors({ origin: 'http:localhost:3000' }))
 // Para que o express entenda requisições no formato JSON - sempre antes das rotas 
 app.use(express.json()); 
 app.use(routes); 
