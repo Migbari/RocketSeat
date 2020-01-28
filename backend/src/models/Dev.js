8b159ca57c46ea279926aaa782ed04e36483ec12
Migbari <webdevel.bin@gmail.com>
2020-01-28 03:35:13 -0300

    Correção bio

--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -8,6 +8,7 @@ const PointSchema = require('./uteis/PointSchema');
 const DevSchema = new mongoose.Schema({
     name: String,
     github_username: String,
+    bio: String,
     avatar_url: String,
     techs: [String],
     location: {
